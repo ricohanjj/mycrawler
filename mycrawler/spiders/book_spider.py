ad9e75144674f484ce0d288534059fe7be52f000
ricohanjj <ricohanjj@163.com>
2018-11-15 17:35:02 +0800

    Initial commit

---
+++ mycrawler/spiders/book_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+from mycrawler.items import Article_DetailItem
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    start_urls = ["http://books.toscrape.com/",]
+
+
+    def parse(self,response):
+        item = Article_DetailItem()
+        article_list = response.xpath('//article[@class="product_pod"]')
+        for article in article_list:
+            title = article.xpath('./h3/a/text()').extract()
+            price  = article.xpath('./div[@class="product_price"]/p[@class="price_color"]/text()').extract()
+
+            item['title'] = title
+            item['price'] = price
+            yield item
+        next_url = response.xpath('//li[@class="next"]//a/@href')[0].extract()
+
+        if next_url:
+            if "catalogue" in next_url:
+                url = str(self.start_urls[0])+str(next_url)
+            else:
+                url = str(self.start_urls[0])+"catalogue/"+str(next_url)
+            print('Debug=========================================', url)
+            yield scrapy.Request(url,callback=self.parse)
